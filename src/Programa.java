71deca0afc8d84b283292f9eb32a2aea73916af9
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-11-03 09:45:35 +0200

    pridetas kintamuju ir isvedimo pavyzdys

--- src/Programa.java
+++ src/Programa.java
@@ -1,8 +1,21 @@
 public class Programa {
     public static void main(String[] args) {
-        System.out.println("Sveiki pasauli");
+        System.out.println("Sveiki pasauli");       // komentaras
+        /*
+        KeliŲ
+        eilučių komentaras
+         */
         System.out.println(5);
 
-
+        // Kintamieji
+        short x = 25;
+        System.out.println("x = " + x);
+        int y = -1515432;
+        byte z = -1;
+        System.out.println("z * y = " + z * y);
+        char raide = 'a';
+        System.out.println("raide = " + raide);
+        String zodis = "bet kas";
+        System.out.println("zodis = " + zodis);
     }
 }
